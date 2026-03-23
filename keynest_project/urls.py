1e98b5205fca52be8a62f1032ec44f7e6a1fda07
Sa22akr <sa22akr@herts.ac.uk>
2026-03-23 05:10:42 +0000

    Remove test route

--- keynest_project/urls.py
+++ keynest_project/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("test/", lambda request: HttpResponse("TEST PAGE WORKS")),
     path("", include("store.urls")),
 ]
