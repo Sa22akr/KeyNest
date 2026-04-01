5a1fd7179684b025e909e3e12320e14ecc0a1c99
Sa22akr <sa22akr@herts.ac.uk>
2026-04-01 13:29:37 +0100

    test payment

--- store/views.py
+++ store/views.py
@@ -66,7 +66,7 @@ def create_checkout_session(request):
                         "product_data": {
                             "name": f"{item['name']} ({item['platform']})"
                         },
-                        "unit_amount": int(float(item["price"]) * 10),
+                        "unit_amount": int(float(item["price"]) * 100),
                     },
                     "quantity": int(item["quantity"]),
                 }
@@ -110,6 +110,7 @@ def verify_session(request):
     except Exception as e:
         print("🔥 VERIFY SESSION ERROR:", repr(e))
         return JsonResponse({"paid": False, "error": str(e)}, status=500)
+
 
 @csrf_exempt
 def submit_order_form(request):
@@ -167,17 +168,13 @@ Submitted Code / Notes:
 {notes}
 """
 
-        try:
-            send_mail(
-                subject="New KeyNest Order Submission",
-                message=message,
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[settings.ORDER_NOTIFICATION_EMAIL],
-                fail_silently=False,
-            )
-            print("✅ Email sent successfully")
-        except Exception as e:
-            print("❌ Email failed:", str(e))
+        send_mail(
+            subject="New KeyNest Order Submission",
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.ORDER_NOTIFICATION_EMAIL],
+            fail_silently=False,
+        )
 
         return JsonResponse(
             {"success": True, "message": "Order submitted successfully."}
